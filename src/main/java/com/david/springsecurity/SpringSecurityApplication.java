b263b9d7dc0782b297271ccff2b2a4fef86d665a
David Mateus <23423789+davidmateusreis@users.noreply.github.com>
2023-07-06 11:41:48 -0300

    JWT

--- src/main/java/com/david/springsecurity/SpringSecurityApplication.java
+++ src/main/java/com/david/springsecurity/SpringSecurityApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.core.oidc.user.OidcUser;
+import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -26,13 +27,27 @@ class HttpController {
 
 	@GetMapping("/private")
 	String privateRoute(@AuthenticationPrincipal OidcUser oidcUser) {
+		return "<h1>Private route, only authorize personal!</h1>";
+	}
+
+	@GetMapping("/cookie")
+	String cookie(@AuthenticationPrincipal OidcUser oidcUser) {
 		return String.format("""
-				<h1>Private route, only authorize personal!</h1>
+				<h1>OAuth 2</h1>
 				<h3>Principal: %s</h3>
 				<h3>Email attribute: %s</h3>
 				<h3>Authorities: %s</h3>
 				<h3>JWT: %s</h3>
 				""", oidcUser, oidcUser.getAttribute("email"), oidcUser.getAuthorities(),
 				oidcUser.getIdToken().getTokenValue());
+	}
+
+	@GetMapping("/jwt")
+	String jwt(@AuthenticationPrincipal Jwt jwt) {
+		return String.format("""
+				<h3>Principal: %s</h3>
+				<h3>Email attribute: %s</h3>
+				<h3>JWT: %s</h3>
+				""", jwt.getClaims(), jwt.getClaim("email"), jwt.getTokenValue());
 	}
 }
