5a539fa4c1c25dd8e7d20b1297a48a37a4d07311
David Mateus <23423789+davidmateusreis@users.noreply.github.com>
2023-07-06 09:11:26 -0300

    Dependency

---
+++ src/main/java/com/david/springsecurity/SecurityConfiguration.java
@@ -0,0 +1,27 @@
+package com.david.springsecurity;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration {
+
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        return httpSecurity
+                .authorizeHttpRequests(
+                        authorizeConfig -> {
+                            authorizeConfig.requestMatchers("/public").permitAll();
+                            authorizeConfig.requestMatchers("/logout").permitAll();
+                            authorizeConfig.anyRequest().authenticated();
+                        })
+                .formLogin(Customizer.withDefaults())
+                .build();
+    }
+
+}
--- src/main/java/com/david/springsecurity/SpringSecurityApplication.java
+++ src/main/java/com/david/springsecurity/SpringSecurityApplication.java
@@ -2,6 +2,8 @@ package com.david.springsecurity;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class SpringSecurityApplication {
@@ -10,4 +12,18 @@ public class SpringSecurityApplication {
 		SpringApplication.run(SpringSecurityApplication.class, args);
 	}
 
+}
+
+@RestController
+class HttpController {
+
+	@GetMapping("/public")
+	String publicRoute() {
+		return "<h1>Public route, feel free to look around!</h1>";
+	}
+
+	@GetMapping("/private")
+	String privateRoute() {
+		return "<h1>Private route, only authorize personal!</h1>";
+	}
 }
